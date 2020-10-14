937d20d50759b79972d4684b854ed1e12de969b5
Rikuto Yamaguchi <yamaguchirkt@gmail.com>
2020-10-14 16:19:18 +0900

    Add iam role

--- main.tf
+++ main.tf
@@ -12,3 +12,30 @@ terraform {
   }
 }
 
+data "aws_iam_policy_document" "lambda_assume_role_document" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+      type = "Service"
+    }
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = "sample-lambda-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_role" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
